bfb8a7ab0f027eb29f4c3b8ed78b1f21e33f7334
Francisco-Hub1 <morenofran234@gmail.com>
2025-04-08 17:18:53 +0200

    Error al comentar la creación del scanner

--- MiAppJava.java
+++ MiAppJava.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class MiAppJava {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        //Scanner scanner = new Scanner(System.in);//
 
         System.out.print("Ingresa tu nombre: ");
         String nombre = scanner.nextLine();  
