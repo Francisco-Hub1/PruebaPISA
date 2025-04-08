fb0a54f52fd1e7323ec49bf4c059af85e6ee4437
Francisco-Hub1 <morenofran234@gmail.com>
2025-04-08 14:19:20 +0200

    Integrar la funcionalidad de saludador

--- MiAppJava.java
+++ MiAppJava.java
@@ -1,18 +1,17 @@
 import java.util.Scanner;
 
-import javax.swing.JOptionPane;
-
 public class MiAppJava {
     public static void main(String[] args) {
-        
-         Scanner scanner = new Scanner(System.in);
+
+        Scanner scanner = new Scanner(System.in);
 
         System.out.print("Ingresa tu nombre: ");
-        String nombre = scanner.nextLine();  // Guarda el nombre ingresado por el usuario
+        String nombre = scanner.nextLine();  
 
-        System.out.println("¡Hola, " + nombre + "!");  // Muestra el nombre en consola
+        Saludador saludador = new Saludador();
+        saludador.saludar(nombre);  
 
         scanner.close();
     }
 }
-    
+
---
+++ Saludador.java
@@ -0,0 +1,6 @@
+public class Saludador {
+    public void saludar(String nombre) {
+        System.out.println("¡Hola, " + nombre + "! Bienvenido a MiAppJava.");
+    }
+}
+
